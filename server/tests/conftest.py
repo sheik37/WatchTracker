40a75e456e0fb1a0f7a80e0b0e9e1b1a94d25855
sheik37 <sheik37.v@gmail.com>
2026-08-19 17:18:35 +0200

    fix: afficher le fichier CRLF dans le check et corriger isort conftest.py

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,5 +1,5 @@
-﻿import sys
-import os
+﻿import os
+import sys
 
 # Ajouter le répertoire server/ au PYTHONPATH pour que les tests trouvent les modules
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
